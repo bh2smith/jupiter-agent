0d17ff575518c8c8719d564501050dbb88490dbc
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-09-23 18:10:52 +0200

    Support Native Asset Sell (#16)
    Utilizes the wrapAndUnwrapSol flag on the Jupiter API.
    
    cf: https://dev.jup.ag/docs/api/swap-api/swap

--- lib/protocol.ts
+++ lib/protocol.ts
@@ -9,6 +9,9 @@ import type {
 import type { ParsedQuoteQuery } from "@/lib/schema";
 import { withErrorHandling } from "@/lib/error";
 
+const NATIVE_ASSET = "So11111111111111111111111111111111111111111";
+const WRAPPED_NATIVE = "So11111111111111111111111111111111111111112";
+
 export class JupiterApi {
   private swapApi: SwapApi;
 
@@ -16,16 +19,34 @@ export class JupiterApi {
     this.swapApi = createJupiterApiClient(apiKey ? { apiKey } : undefined);
   }
 
-  async getQuote(params: QuoteGetRequest): Promise<QuoteResponse> {
-    return withErrorHandling(this.swapApi.quoteGet(params));
+  async getQuote(
+    params: QuoteGetRequest,
+  ): Promise<{ quote: QuoteResponse; wrapAndUnwrapSol: boolean }> {
+    const nativeSellToken = params.inputMint === NATIVE_ASSET;
+    if (nativeSellToken) {
+      // Note that we are modifying the user's input here (but its not used again).
+      // Might be safer to just override the quote parameters without modification.
+      params.inputMint = WRAPPED_NATIVE;
+    }
+    const nativeBuyToken = params.outputMint === NATIVE_ASSET;
+    if (nativeBuyToken) {
+      params.outputMint = WRAPPED_NATIVE;
+    }
+    return {
+      quote: await withErrorHandling(this.swapApi.quoteGet(params)),
+      wrapAndUnwrapSol: nativeSellToken || nativeBuyToken,
+    };
   }
 
   async getSwap(
     userPublicKey: string,
     quote: QuoteResponse,
+    wrapAndUnwrapSol: boolean,
   ): Promise<SwapResponse> {
     // Get serialized transaction
     const swapRequest: SwapRequest = {
+      // https://dev.jup.ag/docs/api/swap-api/swap
+      wrapAndUnwrapSol,
       quoteResponse: quote,
       userPublicKey,
       dynamicComputeUnitLimit: true,
@@ -45,10 +66,18 @@ export class JupiterApi {
     params: ParsedQuoteQuery,
   ): Promise<{ quote: QuoteResponse; swapResponse: SwapResponse }> {
     const { solAddress: userPublicKey, inputMint, outputMint, amount } = params;
-    const quote = await this.getQuote({ inputMint, outputMint, amount });
+    const { quote, wrapAndUnwrapSol } = await this.getQuote({
+      inputMint,
+      outputMint,
+      amount,
+    });
     console.log("Quote:", JSON.stringify(quote, null, 2));
 
-    const swapResponse = await this.getSwap(userPublicKey, quote);
+    const swapResponse = await this.getSwap(
+      userPublicKey,
+      quote,
+      wrapAndUnwrapSol,
+    );
     console.log("SwapTx:", JSON.stringify(swapResponse, null, 2));
     return { quote, swapResponse };
   }
